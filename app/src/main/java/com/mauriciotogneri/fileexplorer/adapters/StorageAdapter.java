9ec615c6a75b850f25f307a3d01e2c9165b3bfe6
mauricio.togneri <mauricio.togneri@gmail.com>
2018-04-03 18:53:19 +0200

    Catching exception

--- app/src/main/java/com/mauriciotogneri/fileexplorer/adapters/StorageAdapter.java
+++ app/src/main/java/com/mauriciotogneri/fileexplorer/adapters/StorageAdapter.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import com.mauriciotogneri.fileexplorer.R;
 import com.mauriciotogneri.fileexplorer.adapters.StorageAdapter.ViewHolder;
 import com.mauriciotogneri.fileexplorer.base.BaseListAdapter;
+import com.mauriciotogneri.fileexplorer.utils.CrashUtils;
 import com.mauriciotogneri.fileexplorer.utils.SpaceFormatter;
 
 public class StorageAdapter extends BaseListAdapter<String, ViewHolder>
@@ -28,18 +29,25 @@ public class StorageAdapter extends BaseListAdapter<String, ViewHolder>
     {
         viewHolder.name.setText(item);
 
-        StatFs stat = new StatFs(item);
-        long blockSize = (long) stat.getBlockSize();
-        long totalSpace = blockSize * stat.getBlockCount();
-        long availableSpace = blockSize * stat.getAvailableBlocks();
+        try
+        {
+            StatFs stat = new StatFs(item);
+            long blockSize = (long) stat.getBlockSize();
+            long totalSpace = blockSize * stat.getBlockCount();
+            long availableSpace = blockSize * stat.getAvailableBlocks();
 
-        SpaceFormatter spaceFormatter = new SpaceFormatter();
+            SpaceFormatter spaceFormatter = new SpaceFormatter();
 
-        String labelTotal = getContext().getString(R.string.space_total);
-        String total = spaceFormatter.format(totalSpace);
-        String labelAvailable = getContext().getString(R.string.space_available);
-        String available = spaceFormatter.format(availableSpace);
-        viewHolder.space.setText(String.format("%s: %s     %s: %s", labelTotal, total, labelAvailable, available));
+            String labelTotal = getContext().getString(R.string.space_total);
+            String total = spaceFormatter.format(totalSpace);
+            String labelAvailable = getContext().getString(R.string.space_available);
+            String available = spaceFormatter.format(availableSpace);
+            viewHolder.space.setText(String.format("%s: %s     %s: %s", labelTotal, total, labelAvailable, available));
+        }
+        catch (Exception e)
+        {
+            CrashUtils.report(e);
+        }
     }
 
     protected static class ViewHolder
