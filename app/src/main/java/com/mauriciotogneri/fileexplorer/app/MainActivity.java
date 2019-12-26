41077213381c72c112806d9a6a627d4e6514b7ba
mauricio.togneri <mauricio.togneri@gmail.com>
2019-12-26 15:36:25 +0100

    Code cleanup

--- app/src/main/java/com/mauriciotogneri/fileexplorer/app/MainActivity.java
+++ app/src/main/java/com/mauriciotogneri/fileexplorer/app/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
     {
         switch (requestCode)
         {
