82c6b320f631c98697d355fc6a966afdcbd54d43
Mauricio Togneri <mauricio.togneri@gmail.com>
2026-05-16 10:17:16 +0200

    Improvements

--- app/src/main/java/com/mauriciotogneri/fileexplorer/ui/theme/Color.kt
+++ app/src/main/java/com/mauriciotogneri/fileexplorer/ui/theme/Color.kt
@@ -3,7 +3,7 @@ package com.mauriciotogneri.fileexplorer.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Primary colors
-val primaryLight = Color(0xFF606060)
+val primaryLight = Color(0xFF606060) // reviewed
 val primaryDark = Color(0xFF909090)
 
 val onPrimaryLight = Color(0xFFFDFBFF)
@@ -23,28 +23,28 @@ val onBackgroundLight = Color(0xFF1A1C1E)
 val onBackgroundDark = Color(0xFFE2E2E6)
 
 // Surface colors
-val surfaceLight = Color(0xFFFBFBFB)
+val surfaceLight = Color(0xFFFBFBFB) // reviewed
 val surfaceDark = Color(0xFF1A1C1E)
 
-val surfaceContainerLowLight = Color(0xFFF0F0F0)
+val surfaceContainerLowLight = Color(0xFFF0F0F0) // reviewed
 val surfaceContainerLowDark = Color(0xFF242628)
 
-val surfaceContainerLight = Color(0xFFF0F0F0)
+val surfaceContainerLight = Color(0xFFF0F0F0) // reviewed
 val surfaceContainerDark = Color(0xFF262829)
 
-val surfaceContainerHighLight = Color(0xFFF0F0F0)
+val surfaceContainerHighLight = Color(0xFFF0F0F0) // reviewed
 val surfaceContainerHighDark = Color(0xFF2A2C2E)
 
-val onSurfaceLight = Color(0xFF202020)
+val onSurfaceLight = Color(0xFF202020) // reviewed
 val onSurfaceDark = Color(0xFFE2E2E6)
 
-val surfaceVariantLight = Color(0xFFD5D5D5)
+val surfaceVariantLight = Color(0xFFD5D5D5) // reviewed
 val surfaceVariantDark = Color(0xFF2F3033)
 
-val onSurfaceVariantLight = Color(0xFF404040)
+val onSurfaceVariantLight = Color(0xFF404040) // reviewed
 val onSurfaceVariantDark = Color(0xFFE2E2E6)
 
-val outlineVariantLight = Color(0xFFC0C0C0)
+val outlineVariantLight = Color(0xFFC0C0C0) // reviewed
 val outlineVariantDark = Color(0xFF404040)
 
 // Error colors
