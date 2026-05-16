cb3e5debc95cdb8025c1f752b8ecb8e24071a6de
Mauricio Togneri <mauricio.togneri@gmail.com>
2026-05-16 10:36:18 +0200

    Improvements

--- app/src/main/java/com/mauriciotogneri/fileexplorer/ui/theme/Color.kt
+++ app/src/main/java/com/mauriciotogneri/fileexplorer/ui/theme/Color.kt
@@ -3,7 +3,6 @@ package com.mauriciotogneri.fileexplorer.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Primary colors
-// spinner, folder/file icons
 val primaryLight = Color(0xFF606060) // reviewed
 val primaryDark = Color(0xFF909090)
 
@@ -36,16 +35,16 @@ val surfaceContainerDark = Color(0xFF262829)
 val surfaceContainerHighLight = Color(0xFFF0F0F0) // reviewed
 val surfaceContainerHighDark = Color(0xFF2A2C2E)
 
-val onSurfaceLight = Color(0xFF202020) // reviewed2
+val onSurfaceLight = Color(0xFF202020) // reviewed
 val onSurfaceDark = Color(0xFFE2E2E6)
 
-val surfaceVariantLight = Color(0xFFD5D5D5) // reviewed2
+val surfaceVariantLight = Color(0xFFD5D5D5) // reviewed
 val surfaceVariantDark = Color(0xFF2F3033)
 
-val onSurfaceVariantLight = Color(0xFF404040) // reviewed2
+val onSurfaceVariantLight = Color(0xFF404040) // reviewed
 val onSurfaceVariantDark = Color(0xFFE2E2E6)
 
-val outlineVariantLight = Color(0xFFC0C0C0) // reviewed2
+val outlineVariantLight = Color(0xFFC0C0C0) // reviewed
 val outlineVariantDark = Color(0xFF404040)
 
 // Error colors
