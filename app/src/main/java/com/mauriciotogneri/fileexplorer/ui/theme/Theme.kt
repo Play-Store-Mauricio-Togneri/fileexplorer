c0ee4238bc7be6bbef9976a799df939b5abc4275
Mauricio Togneri <mauricio.togneri@gmail.com>
2026-05-21 20:20:14 +0200

    Improvements

--- app/src/main/java/com/mauriciotogneri/fileexplorer/ui/theme/Theme.kt
+++ app/src/main/java/com/mauriciotogneri/fileexplorer/ui/theme/Theme.kt
@@ -1,12 +1,9 @@
 package com.mauriciotogneri.fileexplorer.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -14,7 +11,6 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -92,7 +88,6 @@ enum class ThemeMode {
 @Composable
 fun FileExplorerTheme(
     themeMode: ThemeMode = ThemeMode.SYSTEM,
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val darkTheme = when (themeMode) {
@@ -101,14 +96,7 @@ fun FileExplorerTheme(
         ThemeMode.SYSTEM -> isSystemInDarkTheme()
     }
 
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
     val extendedColorScheme = if (darkTheme) DarkExtendedColorScheme else LightExtendedColorScheme
 
