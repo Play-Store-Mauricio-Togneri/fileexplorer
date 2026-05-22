14e7c8ae59a951b43eaf1b1aa6b733d26942193b
Mauricio Togneri <mauricio.togneri@gmail.com>
2026-05-22 21:05:45 +0200

    Improvements

--- app/src/main/java/com/mauriciotogneri/fileexplorer/data/util/AppImageLoader.kt
+++ app/src/main/java/com/mauriciotogneri/fileexplorer/data/util/AppImageLoader.kt
@@ -6,6 +6,8 @@ import coil.ImageLoader
 import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
 import coil.decode.SvgDecoder
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 
 object AppImageLoader {
 
@@ -20,6 +22,17 @@ object AppImageLoader {
 
     private fun buildImageLoader(context: Context): ImageLoader {
         return ImageLoader.Builder(context)
+            .memoryCache {
+                MemoryCache.Builder(context)
+                    .maxSizePercent(0.15)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve("image_cache"))
+                    .maxSizeBytes(50L * 1024 * 1024)
+                    .build()
+            }
             .components {
                 add(PdfThumbnailFetcher.Factory())
                 add(VideoThumbnailFetcher.Factory())
