387b8e4121279fcefd9f7267be454fad76650a12
Mauricio Togneri <mauricio.togneri@gmail.com>
2026-05-23 19:23:27 +0200

    Improvements

--- app/src/main/java/com/mauriciotogneri/fileexplorer/FileExplorerApplication.kt
+++ app/src/main/java/com/mauriciotogneri/fileexplorer/FileExplorerApplication.kt
@@ -6,6 +6,11 @@ import com.mauriciotogneri.fileexplorer.data.repository.PreferencesRepository
 import com.mauriciotogneri.fileexplorer.data.repository.preferencesDataStore
 import com.mauriciotogneri.fileexplorer.data.util.AnalyticsTracker
 import com.mauriciotogneri.fileexplorer.ui.theme.ThemeManager
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class FileExplorerApplication : Application() {
     override fun onCreate() {
@@ -14,5 +19,9 @@ class FileExplorerApplication : Application() {
         val preferencesRepository = PreferencesRepository(preferencesDataStore)
         ThemeManager.setTheme(preferencesRepository.getInitialThemeMode())
         SortManager.setSortMode(preferencesRepository.getInitialSortMode())
+        val exceptionHandler = CoroutineExceptionHandler { _, _ -> }
+        CoroutineScope(SupervisorJob() + Dispatchers.IO + exceptionHandler).launch {
+            AnalyticsTracker.setUserProperties(this@FileExplorerApplication)
+        }
     }
 }
--- app/src/main/java/com/mauriciotogneri/fileexplorer/data/util/AnalyticsTracker.kt
+++ app/src/main/java/com/mauriciotogneri/fileexplorer/data/util/AnalyticsTracker.kt
@@ -1,7 +1,9 @@
 package com.mauriciotogneri.fileexplorer.data.util
 
 import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.Environment
 import com.google.firebase.analytics.FirebaseAnalytics
 
 object AnalyticsTracker {
@@ -32,6 +34,27 @@ object AnalyticsTracker {
 
     fun setUserProperty(key: String, value: String) {
         analytics?.setUserProperty(key, value)
+    }
+
+    fun setUserProperties(context: Context) {
+        setSystemTheme(context)
+        setHasSdCard()
+    }
+
+    private fun setSystemTheme(context: Context) {
+        val nightModeFlags =
+            context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        val theme = when (nightModeFlags) {
+            Configuration.UI_MODE_NIGHT_YES -> "dark"
+            Configuration.UI_MODE_NIGHT_NO -> "light"
+            else -> "unknown"
+        }
+        setUserProperty("system_theme", theme)
+    }
+
+    private fun setHasSdCard() {
+        val hasExternalStorage = Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+        setUserProperty("has_sd_card", hasExternalStorage.toString())
     }
 
     // ---------- Screens ---------- \\
