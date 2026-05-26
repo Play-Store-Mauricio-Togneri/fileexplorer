85e88d4ac625bd3b90845ad2c11e9d492a90bc7e
Mauricio Togneri <mauricio.togneri@gmail.com>
2026-05-26 22:36:57 +0200

    Improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.mauriciotogneri.fileexplorer"
         minSdk = 23
         targetSdk = 36
-        versionCode = 22
-        versionName = "2.0.2"
+        versionCode = 23
+        versionName = "2.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
